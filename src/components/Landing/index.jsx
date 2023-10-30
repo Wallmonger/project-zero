4b8a8f238545ebce2d0c2b6d8410ad66c269ec3a
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-30 17:50:42 +0100

    Wolverine got some claws / btnDisplay

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,7 +1,36 @@
+import { useRef, useEffect, useState } from 'react';
+
 const Landing = () => {
+
+  const refWolverine = useRef(null);
+  const [btn, setBtn] = useState(false);
+
+  console.log(refWolverine);
+
+  useEffect(() => {
+    refWolverine.current.classList.add('startingImg');
+
+    setTimeout(() => {
+      refWolverine.current.classList.remove('startingImg');
+      setBtn(true);
+    }, 1000)
+  }, []);
+
+  const displayBtn = btn && (
+    <>
+       <div className="leftBox">
+          <button className="btn-welcome">Register</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Login</button>
+      </div>
+    </>
+  )
+
+
   return (
-    <main className="welcomePage">
-        
+    <main ref={refWolverine} className="welcomePage">
+        {displayBtn}
     </main>
   )
 }
