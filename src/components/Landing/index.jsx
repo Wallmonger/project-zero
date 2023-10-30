a3943f2cb8211bdf722461734e679ff1365ac6cc
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-30 18:50:40 +0100

    mOver / mOut

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -3,34 +3,46 @@ import { motion } from 'framer-motion';
 
 const Landing = () => {
 
-  const refWolverine = useRef(null);
+  const wRef = useRef(null);
+  
   const [btn, setBtn] = useState(false);
 
-  console.log(refWolverine);
+  console.log(wRef);
 
   useEffect(() => {
-    refWolverine.current.classList.add('startingImg');
+    wRef.current.classList.add('startingImg');
 
     setTimeout(() => {
-      refWolverine.current.classList.remove('startingImg');
+      wRef.current.classList.remove('startingImg');
       setBtn(true);
     }, 1000)
   }, []);
 
+  const setLeftImg = () => wRef.current.classList.add('leftImg');
+  const setRightImg = () => wRef.current.classList.add('rightImg');
+  const clearImg = () => {
+    if (wRef.current.classList.contains('leftImg'))
+        wRef.current.classList.remove('leftImg');
+    else if (wRef.current.classList.contains('rightImg'))
+        wRef.current.classList.remove('rightImg');
+  }
+  
+  
+
   const displayBtn = btn && (
     <>
-       <div className="leftBox">
-          <button className="btn-welcome">Register</button>
+       <div className="leftBox" onMouseOver={setLeftImg} onMouseOut={clearImg}>
+          <motion.button className="btn-welcome" whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9}}>Register</motion.button>
       </div>
-      <div className="rightBox">
-        <button className="btn-welcome">Login</button>
+      <div className="rightBox" onMouseOver={setRightImg} onMouseOut={clearImg}>
+        <motion.button className="btn-welcome" whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9}}>Login</motion.button>
       </div>
     </>
   )
 
 
   return (
-    <main ref={refWolverine} className="welcomePage">
+    <main ref={wRef} className="welcomePage">
         {displayBtn}
     </main>
   )
