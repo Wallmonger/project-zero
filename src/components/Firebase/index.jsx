53f02cb64619f019f1e8bf9b46a2a72442a0f3ca
Wallmonger <nicolas.berrier360@gmail.com>
2023-11-04 22:26:33 +0100

    Signup form

--- src/components/Firebase/index.jsx
+++ src/components/Firebase/index.jsx
@@ -1,5 +1,5 @@
-import Firebase from './firebase';
-import FirebaseContext from './context';
+// import Firebase from './firebase';
+// import FirebaseContext from './context';
 
-export default Firebase;
-export default { FirebaseContext };
+// export default Firebase;
+// export default { FirebaseContext };
--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,8 +1,58 @@
+import { useState } from 'react'; 
+
 const Signup = () => {
+
+  const data = {
+    pseudo: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+  const [loginData, setloginData] = useState(data);
+
+  const handleChange = (e) => {
+   
+    setloginData({
+      ...loginData,
+      pseudo: e.target.value,
+    });
+    console.log(loginData);
+  }
+  
+  const { pseudo, email, password, confirmPassword } = loginData;
+
   return (
     <div className="signUpLoginBox">
         <div className="slContainer">
-            Signup
+            <div className="formBoxLeftSignup">
+
+            </div>
+            <div className="formBoxRight">
+              <div className="formContent">
+                  <h2>Sign Up</h2>
+                  <form>
+                    <div className="inputBox">
+                      <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required />
+                      <label htmlFor="pseudo">Pseudo</label>
+                    </div>
+
+                    <div className="inputBox">
+                      <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
+                      <label htmlFor="email">Email</label>
+                    </div>
+
+                    <div className="inputBox">
+                      <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
+                      <label htmlFor="password">Password</label>
+                    </div>
+
+                    <div className="inputBox">
+                      <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
+                      <label htmlFor="confirmPassword">Confirm password</label>
+                    </div>
+                  </form>
+              </div>
+            </div>
         </div>
     </div>
   )
